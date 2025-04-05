2cfac4daf772af2525d08f9eabc604d24db36b42
Kent Delante <kent.delante@proton.me>
2025-04-05 13:41:50 +0800

    Format license headers

--- src/main.rs
+++ src/main.rs
@@ -14,8 +14,7 @@
 // GNU General Public License for more details.
 
 // You should have received a copy of the GNU General Public License
-// along with Based. If not, see <https://www.gnu.org/licenses/>. 
-
+// along with Based. If not, see <https://www.gnu.org/licenses/>.
 
 mod application;
 #[rustfmt::skip]
--- src/modals/mod.rs
+++ src/modals/mod.rs
@@ -14,7 +14,6 @@
 // GNU General Public License for more details.
 
 // You should have received a copy of the GNU General Public License
-// along with Based. If not, see <https://www.gnu.org/licenses/>. 
-
+// along with Based. If not, see <https://www.gnu.org/licenses/>.
 
 pub mod connection;
